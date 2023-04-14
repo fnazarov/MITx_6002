55ce6b99c553ff2775b6a2254ec36d500b80bcce
Farid Nazarov <faridnazarov@gmail.com>
2023-04-14 10:51:25 +0200

    UNIT 1 Lecture 3

--- Unit1/Lecture 1/Lecture3.py
+++ Unit1/Lecture 1/Lecture3.py
@@ -58,8 +58,24 @@ class Digraph(object):
         return result[:-1] # omit final newline
 
 class Graph(Digraph):
+    """Graph does not have directionality associated with an edge. Edge allow passage in either direction"""
     def addEdge(self,edge):
         Digraph.addEdge(self,edge)
         rev = Edge(edge.getDestionation(), edge.getSource())
         Digraph.addEdge(self,rev)
 
+def buildCityGraph(graphType):
+    g = graphType()
+    for name in ('Boston', 'Providence', 'New York', 'Chicago',
+                 'Denver', 'Phoenix', 'Los Angeles'): #Create 7 nodes
+        g.addNode(Node(name))
+    g.addEdge(Edge(g.getNode('Boston'), g.getNode('Providence')))
+    g.addEdge(Edge(g.getNode('Boston'), g.getNode('New York')))
+    g.addEdge(Edge(g.getNode('Providence'), g.getNode('Boston')))
+    g.addEdge(Edge(g.getNode('Providence'), g.getNode('New York')))
+    g.addEdge(Edge(g.getNode('New York'), g.getNode('Chicago')))
+    g.addEdge(Edge(g.getNode('Chicago'), g.getNode('Denver')))
+    g.addEdge(Edge(g.getNode('Denver'), g.getNode('Phoenix')))
+    g.addEdge(Edge(g.getNode('Denver'), g.getNode('New York')))
+    g.addEdge(Edge(g.getNode('Los Angeles'), g.getNode('Boston')))
+    return g
