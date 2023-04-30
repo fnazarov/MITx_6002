19288918fe798cba7232431fc765d03091bd861e
Farid Nazarov <faridnazarov@gmail.com>
2023-04-30 17:17:23 +0200

    Monte Carlo Simulation for Square Root of two

--- UNIT 2/Lecture 8/Lect 8 Exercises.py
+++ UNIT 2/Lecture 8/Lect 8 Exercises.py
@@ -46,3 +46,12 @@ pylab.title('Rolling Continuous Dice')
 pylab.xlabel('Value')
 pylab.ylabel('Probability')
 pylab.legend()
+
+def throwNeedles(numNeedles):
+    success = 0
+    for n in range(numNeedles):
+        x = random.random()
+        if (1+x)**2 < 2.0:
+            success += 1
+    sqrt2 = 1+(float(success)/numNeedles)
+    return sqrt2
