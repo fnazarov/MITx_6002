e9d12c66a0f208abe138d9e0528b9ba9ef1d0e5e
Farid Nazarov <faridnazarov@gmail.com>
2023-04-30 09:56:19 +0200

    Dealing with Distributions

--- UNIT 2/Lecture 8/Lect 8 Exercises.py
+++ UNIT 2/Lecture 8/Lect 8 Exercises.py
@@ -18,4 +18,31 @@ def check_empirical (mu_max, sigma_max, num_trials):
             area = scipy.integrate.quad(gaussian, mu-num_std*sigma, mu + num_std*sigma,(mu, sigma))[0]
             print(' Fraction within ', num_std, 'std =', round(area,4))
 
-check_empirical(10,10,3)
+def getMeanAndStd(X):
+    mean = sum(X)/float(len(X))
+    tot = 0.0
+    for x in X:
+        tot += (x - mean)**2
+    std = (tot/len(X))**0.5
+    return mean, std
+
+def plotMeans(numDice, numRolls, numBins, legend, color, style):
+    means =[]
+    for i in range(numRolls//numDice):
+        vals = 0
+        for j in range(numDice):
+            vals += 5 * random.random()
+        means.append(vals/float(numDice))
+    pylab.hist(means, numBins, color= color, label = legend,
+               weights = pylab.array(len(means)*[1])/len(means),
+               hatch = style)
+    return getMeanAndStd(means)
+
+mean, std = plotMeans(1, 1000000, 19, '1 die', 'b', '*')
+print('Mean of rolling 1 die =', str(mean) + ',', 'Std =', std)
+mean, std = plotMeans(50, 1000000, 19, 'Mean of 50 dice', 'r', '//')
+print('Mean of rolling 50 dice =', str(mean) + ',', 'Std =', std)
+pylab.title('Rolling Continuous Dice')
+pylab.xlabel('Value')
+pylab.ylabel('Probability')
+pylab.legend()
